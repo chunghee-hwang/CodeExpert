dd90937af9d956de634ade20e4c6ac0ef7d49263
Chunghee Hwang <hch0821@naver.com>
2020-08-18 18:08:43 +0000

    Update: Frontend - Store logger 제거

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import modules, { rootSaga } from './modules';
-import logger from 'redux-logger';
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(modules, applyMiddleware(logger, sagaMiddleware));
+const store = createStore(modules, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 export default store;

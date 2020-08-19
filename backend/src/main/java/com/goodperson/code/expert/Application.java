125ee7ee1e8e16730fda147b803f67cb699e2b60
Chunghee Hwang <hch0821@naver.com>
2020-08-19 16:52:21 +0000

    Update: Backend - 타임존 - Asia/Seoul로 설정

--- backend/src/main/java/com/goodperson/code/expert/Application.java
+++ backend/src/main/java/com/goodperson/code/expert/Application.java
@@ -1,5 +1,9 @@
 package com.goodperson.code.expert;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
+	@PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
